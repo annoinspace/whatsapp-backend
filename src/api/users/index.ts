173ec4ddf876b9a5baaeea5cd5cd39b192e5d7b6
annoinspace <aneesahalmask@gmail.com>
2023-02-23 12:22:27 +0000

    package changes stashed

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,185 +1,166 @@
-import express from "express";
-import createHttpError from "http-errors";
-import { JwtAuthenticationMiddleware, UserRequest } from "../../lib/jwtAuth";
-import UsersModel from "./model";
-import { v2 as cloudinary } from "cloudinary";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import multer from "multer";
-import { createAccessToken } from "../../lib/tools";
+import express from "express"
+import createHttpError from "http-errors"
+import { JwtAuthenticationMiddleware, UserRequest } from "../../lib/jwtAuth"
+import UsersModel from "./model"
+import { v2 as cloudinary } from "cloudinary"
+import { CloudinaryStorage } from "multer-storage-cloudinary"
+import multer from "multer"
+import { createAccessToken } from "../../lib/tools"
 
 const cloudinaryUploader = multer({
   storage: new CloudinaryStorage({
     cloudinary,
+    params: {
+      folder: "whatsapp"
+    }
   }),
-  limits: { fileSize: 1024 * 1024 },
-}).single("userProfile");
+  limits: { fileSize: 1024 * 1024 }
+}).single("image")
 
-const usersRouter = express.Router();
+const usersRouter = express.Router()
 
 //1. GET USERS (EITHER ALL OF THEM OR BY EMAIL)
 usersRouter.get("/", JwtAuthenticationMiddleware, async (req, res, next) => {
   try {
-    let users;
+    let users
     if (req.query.search) {
       users = await UsersModel.find({
         $or: [
           { username: { $regex: req.query.search, $options: "i" } },
-          { email: { $regex: req.query.search, $options: "i" } },
-        ],
-      });
+          { email: { $regex: req.query.search, $options: "i" } }
+        ]
+      })
     } else {
-      users = await UsersModel.find();
+      users = await UsersModel.find()
     }
 
     if (users) {
-      res.status(200).send(users);
+      res.status(200).send(users)
     } else {
-      next(createHttpError(404, "No users were found."));
+      next(createHttpError(404, "No users were found."))
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 //2. GET ME
 
-usersRouter.get(
-  "/me",
-  JwtAuthenticationMiddleware,
-  async (req: UserRequest, res, next) => {
-    try {
-      if (req.user) {
-        const me = await UsersModel.findById(req.user._id);
-        if (me) {
-          res.send(me);
-        }
-      } else {
-        createHttpError(404, "user not found");
+usersRouter.get("/me", JwtAuthenticationMiddleware, async (req: UserRequest, res, next) => {
+  try {
+    if (req.user) {
+      const me = await UsersModel.findById(req.user._id)
+      if (me) {
+        res.send(me)
       }
-    } catch (error) {
-      next(error);
+    } else {
+      createHttpError(404, "user not found")
     }
+  } catch (error) {
+    next(error)
   }
-);
+})
 
 //3. EDIT ME
 
-usersRouter.put(
-  "/me",
-  JwtAuthenticationMiddleware,
-  async (req: UserRequest, res, next) => {
-    try {
-      if (req.user) {
-        const updatedUser = await UsersModel.findByIdAndUpdate(
-          req.user._id,
-          req.body,
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
-        res.send(updatedUser);
-      }
-    } catch (error) {
-      next(error);
+usersRouter.put("/me", JwtAuthenticationMiddleware, async (req: UserRequest, res, next) => {
+  try {
+    if (req.user) {
+      const updatedUser = await UsersModel.findByIdAndUpdate(req.user._id, req.body, {
+        new: true,
+        runValidators: true
+      })
+      res.send(updatedUser)
     }
+  } catch (error) {
+    next(error)
   }
-);
+})
 
 //4. CHANGE MY AVATAR
 
-usersRouter.post(
-  "/me/avatar",
-  JwtAuthenticationMiddleware,
-  cloudinaryUploader,
-  async (req: UserRequest, res, next) => {
-    try {
-      if (req.user) {
-        const foundUser = await UsersModel.findByIdAndUpdate(
-          req.user._id,
-          { avatar: req.file!.path },
-          { new: true, runValidators: true }
-        );
-        res.status(201).send({ message: "User Pic Uploaded" });
-      } else {
-        next(createHttpError(404, `User with id is not found`));
+usersRouter.post("/me/avatar", JwtAuthenticationMiddleware, cloudinaryUploader, async (req: UserRequest, res, next) => {
+  try {
+    console.log(req.file)
+    if (req.user) {
+      const foundUser = await UsersModel.findByIdAndUpdate(
+        req.user._id,
+        { avatar: req.file!.path },
+        { new: true, runValidators: true }
+      )
+      if (foundUser) {
+        res.status(201).send({ message: "User Pic Uploaded" })
       }
-    } catch (error) {
-      next(error);
+    } else {
+      next(createHttpError(404, `User with id is not found`))
     }
+  } catch (error) {
+    next(error)
   }
-);
+})
 
 //5. GET SPECIFIC USER
 
-usersRouter.get(
-  "/:userId",
-  JwtAuthenticationMiddleware,
-  async (req, res, next) => {
-    try {
-      const foundUser = await UsersModel.findById(req.params.userId);
-      if (foundUser) {
-        res.status(200).send(foundUser);
-      } else {
-        createHttpError(404, `user with id ${req.params.userId} not found `);
-      }
-    } catch (error) {
-      next(error);
+usersRouter.get("/:userId", JwtAuthenticationMiddleware, async (req, res, next) => {
+  try {
+    const foundUser = await UsersModel.findById(req.params.userId)
+    if (foundUser) {
+      res.status(200).send(foundUser)
+    } else {
+      createHttpError(404, `user with id ${req.params.userId} not found `)
     }
+  } catch (error) {
+    next(error)
   }
-);
+})
 
 //6. REGISTER USER
 
 usersRouter.post("/register", async (req, res, next) => {
   try {
-    const newUser = new UsersModel(req.body);
-    const { _id } = await newUser.save();
+    const newUser = new UsersModel(req.body)
+    const { _id } = await newUser.save()
 
-    res.status(201).send({ _id });
+    res.status(201).send({ _id })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 //7. LOGIN USER
 
 usersRouter.post("/login", async (req, res, next) => {
   try {
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
-    const user = await UsersModel.checkCredentials(email, password);
+    const user = await UsersModel.checkCredentials(email, password)
 
     if (user) {
-      const payload = { _id: user._id };
-      const accessToken = await createAccessToken(payload);
-      res.send({ accessToken });
+      const payload = { _id: user._id }
+      const accessToken = await createAccessToken(payload)
+      res.send({ accessToken })
     } else {
-      next(createHttpError(401, `Credentials are not ok!`));
+      next(createHttpError(401, `Credentials are not ok!`))
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 //8. LOGOUT USER
 
-usersRouter.delete(
-  "/session",
-  JwtAuthenticationMiddleware,
-  async (req: UserRequest, res, next) => {
-    try {
-      if (req.user) {
-        const user = await UsersModel.updateOne({ id: req.user._id });
+usersRouter.delete("/session", JwtAuthenticationMiddleware, async (req: UserRequest, res, next) => {
+  try {
+    if (req.user) {
+      const user = await UsersModel.updateOne({ id: req.user._id })
 
-        if (user) {
-          res.status(200).send({ message: "User logged out" });
-        }
+      if (user) {
+        res.status(200).send({ message: "User logged out" })
       }
-    } catch (error) {
-      next(error);
     }
+  } catch (error) {
+    next(error)
   }
-);
+})
 
-export default usersRouter;
+export default usersRouter
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,39 @@
-import express from "express";
-import cors from "cors";
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
 import {
   forbiddenErrorHandler,
   genericErrorHandler,
   notFoundErrorHandler,
-  unauthorizedErrorHandler,
-} from "./errorHandlers";
-import usersRouter from "./api/users";
-import chatsRouter from "./api/chats";
-import { Server as SocketIOServer } from "socket.io";
-import { createServer } from "http";
-import { newConnectionHandler } from "./socket";
+  unauthorizedErrorHandler
+} from "./errorHandlers"
+import usersRouter from "./api/users"
+import chatsRouter from "./api/chats"
+import { Server as SocketIOServer } from "socket.io"
+import { createServer } from "http"
+import { newConnectionHandler } from "./socket"
 
-export const server = express();
+export const server = express()
 
 // *******************************SOCKET>IO*****************************
-const httpServer = createServer(server);
-export const io = new SocketIOServer(httpServer);
+const httpServer = createServer(server)
+export const io = new SocketIOServer(httpServer)
 
-io.on("connection", newConnectionHandler);
+io.on("connection", newConnectionHandler)
 
 // ***************************** MIDDLEWARES ***************************
-server.use(cors());
-server.use(express.json());
+server.use(cors())
+
+server.use(express.json())
 
 // ****************************** ENDPOINTS ****************************
-server.use("/users", usersRouter);
-server.use("/chats", chatsRouter);
+server.use("/users", usersRouter)
+server.use("/chats", chatsRouter)
 
 // *************************** ERROR HANDLERS **************************
-server.use(unauthorizedErrorHandler);
-server.use(forbiddenErrorHandler);
-server.use(notFoundErrorHandler);
-server.use(genericErrorHandler);
+server.use(unauthorizedErrorHandler)
+server.use(forbiddenErrorHandler)
+server.use(notFoundErrorHandler)
+server.use(genericErrorHandler)
 
-export default httpServer;
+export default httpServer
