5868db748d2eba50ecb8121a5bfed9f2a07b57d5
annoinspace <aneesahalmask@gmail.com>
2023-02-23 16:14:18 +0000

    commenting out the changes

--- src/api/chats/index.ts
+++ src/api/chats/index.ts
@@ -1,61 +1,84 @@
-import express from "express";
-import UsersModel from "../users/model";
-import { JwtAuthenticationMiddleware, UserRequest } from "../../lib/jwtAuth";
-import ChatsModel from "./model";
-import createHttpError from "http-errors";
+import express from "express"
+import UsersModel from "../users/model"
+import { JwtAuthenticationMiddleware, UserRequest } from "../../lib/jwtAuth"
+import ChatsModel from "./model"
+import createHttpError from "http-errors"
 
-const chatsRouter = express.Router();
+const chatsRouter = express.Router()
 
 // CHATS
 
-// Create a chat with other participant(s)
+// // Create a chat with other participant(s)
+// chatsRouter.post("/openChat", JwtAuthenticationMiddleware, async (req, res, next) => {
+//   try {
+//     const { userIds } = req.body
+//     console.log("userIds")
+//     // Check if a ChatsModel with the given userIds already exists
+//     const existingChat = await ChatsModel.findOne({ members: { $all: userIds } })
+//     if (existingChat) {
+//       console.log("chatroom already exists")
+//       // Return the existing ChatsModel's _id
+//       res.status(200).send({ message: "chatroom already exists", chatroomId: existingChat._id })
+//     } else {
+//       // Creating a new ChatsModel with the array of ids
+//       const { _id } = await ChatsModel.create({ userIds })
+//       console.log("chatroom created")
+
+//       // Add the new chatroom to the "chats" array for each user
+//       for (let userId of userIds) {
+//         await UsersModel.findByIdAndUpdate(userId, { $push: { chats: _id } })
+//       }
+
+//       res.status(201).send({ message: "chatroom created", chatroomId: _id })
+//     }
+//   } catch (error) {
+//     console.log(error)
+//     next(error)
+//   }
+// })
 
 chatsRouter.post("/", JwtAuthenticationMiddleware, async (req, res, next) => {
   try {
-    const newChat = new ChatsModel(req.body);
-    const { _id } = await newChat.save();
-    console.log("newChat");
+    const newChat = new ChatsModel(req.body)
+    const { _id } = await newChat.save()
+    console.log("newChat")
     if (_id) {
-      res.status(201).send(_id);
+      res.status(201).send(_id)
     } else {
-      next(createHttpError(404, `the chat did not create`));
+      next(createHttpError(404, `the chat did not create`))
     }
   } catch (error) {
-    console.log(error);
-    next(error);
+    console.log(error)
+    next(error)
   }
-});
+})
 
 // GET all chats
 
-chatsRouter.get(
-  "/",
-  JwtAuthenticationMiddleware,
-  async (req: UserRequest, res, next) => {
-    try {
-      const chats = await ChatsModel.find();
-      res.send(chats);
-    } catch (error) {
-      next(error);
-    }
+chatsRouter.get("/", JwtAuthenticationMiddleware, async (req: UserRequest, res, next) => {
+  try {
+    const chats = await ChatsModel.find()
+    res.send(chats)
+  } catch (error) {
+    next(error)
   }
-);
+})
 
 // GET full message history for specific chat
 
 chatsRouter.get("/:id", JwtAuthenticationMiddleware, async (req, res, next) => {
   try {
-    const chat = await ChatsModel.findById(req.params.id).populate("members");
-    console.log(chat);
+    const chat = await ChatsModel.findById(req.params.id).populate("members")
+    console.log(chat)
     if (chat) {
-      res.send(chat);
+      res.send(chat)
     } else {
-      createHttpError(404, `chat with the id ${req.params.id} not found`);
+      createHttpError(404, `chat with the id ${req.params.id} not found`)
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 // check if chat exists
 // chatsRouter.get(
@@ -77,4 +100,4 @@ chatsRouter.get("/:id", JwtAuthenticationMiddleware, async (req, res, next) => {
 //   }
 // );
 
-export default chatsRouter;
+export default chatsRouter
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,72 +1,74 @@
-import mongoose, { ObjectId } from "mongoose";
-import { Message } from "../api/chats/types";
-import ChatsModel from "../api/chats/model";
+import mongoose, { ObjectId } from "mongoose"
+import { Message } from "../api/chats/types"
+import ChatsModel from "../api/chats/model"
 
 interface Users {
-  id: ObjectId;
-  socketId: string;
-  userName: string;
+  id: ObjectId
+  socketId: string
+  userName: string
 }
 
 interface UsernameWithId {
-  username: string;
-  _id: ObjectId;
+  username: string
+  _id: ObjectId
 }
 
-let OnlineUsers: Users[] = [];
+let OnlineUsers: Users[] = []
 
 export const newConnectionHandler = (newUser: any) => {
-  console.log("SocketId: ", newUser.id);
+  console.log("SocketId: ", newUser.id)
 
   newUser.on("connectReceiveInfo", (payload: UsernameWithId) => {
     OnlineUsers.push({
       id: payload._id,
       socketId: newUser.id,
-      userName: payload.username,
-    });
-    console.log("Current online users", OnlineUsers);
+      userName: payload.username
+    })
+    console.log("Current online users", OnlineUsers)
 
-    newUser.emit("signedIn", OnlineUsers);
-    newUser.broadcast.emit("newConnection", OnlineUsers);
+    newUser.emit("signedIn", OnlineUsers)
+    newUser.broadcast.emit("newConnection", OnlineUsers)
 
     newUser.on("checkChats", async (userIds: string[]) => {
       try {
-        const chat = await ChatsModel.findOne({where:{members:{$in:userIds}}});      
+        const chat = await ChatsModel.findOne({ where: { members: { $in: userIds } } })
 
         if (chat) {
-          newUser.emit("existingChat", chat._id);
-          console.log("Found a chat");
+          newUser.emit("existingChat", chat._id)
+          console.log("Found a chat")
         } else {
-          newUser.emit("noExistingChat");
-          console.log("Found no chats");
+          newUser.emit("noExistingChat")
+          console.log("Found no chats")
+          // const { _id } = await ChatsModel.create({  OnlineUsers })
+          // console.log("chatroom created")
         }
       } catch (error) {
-        console.log(error);
-        newUser.emit("errorCheckingChats", error);
+        console.log(error)
+        newUser.emit("errorCheckingChats", error)
       }
-    });
+    })
 
     newUser.on("openChat", (payload: string) => {
-      const roomId: string = payload;
+      const roomId: string = payload
 
-      const messages: Message[] = [];
+      const messages: Message[] = []
 
-      newUser.join(roomId);
+      newUser.join(roomId)
 
       newUser.on("sendMessage", async (message: Message) => {
         const chat = await ChatsModel.findByIdAndUpdate(roomId, {
-          $push: { messages: message },
-        });
+          $push: { messages: message }
+        })
 
-        newUser.to(roomId).emit("newMessage", message);
-      });
+        newUser.to(roomId).emit("newMessage", message)
+      })
 
       newUser.on("disconnect", () => {
         const newOnlineUsers = OnlineUsers.filter((user) => {
-          user.socketId !== newUser.id;
-        });
-        newUser.emit("newConnection", newOnlineUsers);
-      });
-    });
-  });
-};
+          user.socketId !== newUser.id
+        })
+        newUser.emit("newConnection", newOnlineUsers)
+      })
+    })
+  })
+}
